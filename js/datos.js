e9506ab7234953ec94bdb1157d7d3a7679a2d7a9
Alvaro3c <alvaro.cuallado.g@gmail.com>
2022-10-22 14:34:29 +0200

    datosjs. Objeto creado

---
+++ js/datos.js
@@ -0,0 +1,29 @@
+let toDoTasks = new Array
+
+toDoTasks = [
+    {
+        'idtask': 0,
+        'title': 'estudiar javascript',
+        'priority': 'urgente'
+    },
+
+    {
+        'idtask': 1,
+        'title': 'pasear al perro',
+        'priority': 'diaria'
+    },
+
+    {
+        'idtask': 2,
+        'title': 'regar plantas',
+        'priority': 'semanal'
+    },
+
+    {
+        'idtask': 3,
+        'title': 'pagar impuestos',
+        'priority': 'urgente'
+    }
+
+
+]
